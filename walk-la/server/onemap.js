85f478a957961a5c3c0be4e2ed68a0f60a748d25
kaizey-l <kaylenelimyx@gmail.com>
2026-09-19 11:26:36 +0800

    Integrate weather router into OneMap server

--- walk-la/server/onemap.js
+++ walk-la/server/onemap.js
@@ -1,14 +1,13 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-
-console.log("OneMap email loaded:", !!process.env.ONEMAP_EMAIL);
-console.log("OneMap password loaded:", !!process.env.ONEMAP_PASSWORD);
+import weatherRouter from "./weather.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/weather", weatherRouter);
 
 const ONEMAP_BASE_URL = "https://www.onemap.gov.sg";
 
